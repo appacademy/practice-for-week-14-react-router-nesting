d4bc4d8db414d98bc2bf10e9491283e6d7e7f63c
Marcel Reig <marcelreig@gmail.com>
2023-02-23 15:33:06 +0000

    Update 3 files

--- src/components/MovieDetails/index.js
+++ src/components/MovieDetails/index.js
@@ -1,7 +1,15 @@
-function MovieDetails() {
+import { useParams } from "react-router-dom";
+
+function MovieDetails({ movies }) {
+  const { movieId } = useParams();
+  console.log(movieId);
+
+  const movieChoice = movies.find((movie) => movie.id === movieId);
+
+  console.log(movieChoice);
   return (
-    <div className='comp purple'>
-      <h1>MovieDetails Component</h1>
+    <div className="comp purple">
+      <h1>patatas</h1>
     </div>
   );
 }
--- src/components/Movies/index.js
+++ src/components/Movies/index.js
@@ -1,7 +1,22 @@
-function Movies() {
+import { NavLink, Route, Switch } from "react-router-dom";
+import MovieDetails from "../MovieDetails/";
+
+function Movies(props) {
+  const { movies } = props;
+
   return (
-    <div className='comp orange'>
+    <div className="comp orange">
       <h1>Movies Component</h1>
+      <nav>
+        {movies.map((movie) => {
+          return <NavLink to={`/movies/${movie.id}`}>{movie.title}</NavLink>;
+        })}
+      </nav>
+      <Switch>
+        <Route path="/movies/:movieId">
+          <MovieDetails movies={movies} />
+        </Route>
+      </Switch>
     </div>
   );
 }
